72c6f26bb0c20359b87ff8cfaab0bf551a3dd3a4
Jack Matthews <jackmatt2@gmail.com>
2014-10-22 21:27:53 +1100

    Update EmberModel

--- src/main/java/au/com/emberspring/ember/EmberModel.java
+++ src/main/java/au/com/emberspring/ember/EmberModel.java
@@ -12,61 +12,73 @@ import com.google.common.base.CaseFormat;
 
 @SuppressWarnings("serial")
 public final class EmberModel extends ConcurrentHashMap<String, Object> {
-	
-	private EmberModel() {
-		//Must use the builder
-	}
-	
-	public static class Builder implements au.com.emberspring.patterns.Builder<EmberModel> {
 
-		private ConcurrentMap<String, Object> sideLoadedItems = new ConcurrentHashMap<String, Object>();
-		private ConcurrentMap<String, Object> metaData = new ConcurrentHashMap<String, Object>(); 
-		
-		public Builder(Object entity) {
-			Assert.notNull(entity);
-			sideLoad(entity);
-		}
-		
-		public Builder(Class<?> clazz, Collection<?> entities) {
-			Assert.notNull(entities);
-			sideLoad(clazz, entities);
-		}
-		
-		public Builder addMeta(String key, Object value) {
-			metaData.put(key, value);
-			return this;
-		}
-		
-		public Builder sideLoad(Object entity) {
-			sideLoadedItems.put(getSingularName(entity.getClass()), entity);
-			return this;
-		}
-		
-		public Builder sideLoad(Class<?> clazz, Collection<?> entities) {
-			sideLoadedItems.put(getPluralName(clazz), entities);
-			return this;
-		}
-		
-		private final String getSingularName(Class<?> clazz) {
-			Assert.notNull(clazz);
-			return CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_CAMEL, clazz.getSimpleName());
-		}
-		
-		private final String getPluralName(Class<?> clazz) {
-			return English.plural(getSingularName(clazz));
-		}
-		
-		@Override
-		public EmberModel build() {
-			
-			if(metaData.size() > 0) {
-				sideLoadedItems.put("meta", metaData);				
-			}
-			
-			EmberModel sideLoader = new EmberModel();
-			sideLoader.putAll(sideLoadedItems);
-			return sideLoader;
-		}
-		
-	}
+    private EmberModel() {
+        //Must use the builder
+    }
+
+    public static class Builder implements au.com.emberspring.patterns.Builder<EmberModel> {
+        private final ConcurrentMap<String, Object> sideLoadedItems = new ConcurrentHashMap<String, Object>();
+        private final ConcurrentMap<String, Object> metaData = new ConcurrentHashMap<String, Object>();
+
+        public Builder(final Object entity) {
+            Assert.notNull(entity);
+            sideLoad(entity);
+        }
+
+        public Builder(final Class<?> clazz, final Collection<?> entities) {
+            Assert.notNull(entities);
+            sideLoad(clazz, entities);
+        }
+
+        public Builder(final String rootName, final Collection<?> entities) {
+            Assert.notNull(entities);
+            sideLoad(rootName, entities);
+        }
+
+        public Builder addMeta(final String key, final Object value) {
+            metaData.put(key, value);
+            return this;
+        }
+
+        public Builder sideLoad(final Object entity) {
+            if (entity != null) {
+                sideLoadedItems.put(getSingularName(entity.getClass()), entity);
+            }
+            return this;
+        }
+
+        public Builder sideLoad(final Class<?> clazz, final Collection<?> entities) {
+            if (entities != null) {
+                sideLoadedItems.put(getPluralName(clazz), entities);
+            }
+            return this;
+        }
+
+        public Builder sideLoad(final String rootName, final Collection<?> entities) {
+            if (entities != null) {
+                sideLoadedItems.put(English.plural(rootName), entities);
+            }
+            return this;
+        }
+
+        private String getSingularName(final Class<?> clazz) {
+            Assert.notNull(clazz);
+            return CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_CAMEL, clazz.getSimpleName());
+        }
+
+        private String getPluralName(final Class<?> clazz) {
+            return English.plural(getSingularName(clazz));
+        }
+
+        @Override
+        public EmberModel build() {
+            if (metaData.size() > 0) {
+                sideLoadedItems.put("meta", metaData);
+            }
+            EmberModel sideLoader = new EmberModel();
+            sideLoader.putAll(sideLoadedItems);
+            return sideLoader;
+        }
+    }
 }
